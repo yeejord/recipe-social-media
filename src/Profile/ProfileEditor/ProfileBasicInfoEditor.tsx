e050093a6635343fff49e1cec7314fac3fa64df8
yeejord <147876292+yeejord@users.noreply.github.com>
2025-03-13 21:39:05 -0400

    updates

---
+++ src/Profile/ProfileEditor/ProfileBasicInfoEditor.tsx
@@ -0,0 +1,43 @@
+import { Row, Image, Card, InputGroup, Form } from "react-bootstrap";
+import { FaCameraRetro } from "react-icons/fa";
+
+export default function ProfileBasicInfoEditor() {
+  return (
+    <div id="recipe-profile-basic-info">
+      <Row>
+        <div className="profile-card-holder">
+          <Card className="p-2">
+            <InputGroup className="mb-3" size="lg">
+              <Form.Control
+                placeholder="Name"
+                aria-label="Name"
+                defaultValue={"Name"}
+              />
+            </InputGroup>
+            <InputGroup className="mb-3">
+              <Form.Control
+                placeholder="Username"
+                aria-label="Username"
+                defaultValue={"username"}
+              />
+            </InputGroup>
+          </Card>
+        </div>
+      </Row>
+      <Row className="profile-image-holder">
+        <div className="profile-image-edit-holder">
+          <Image
+            src="../../images/DetectiveClock.png"
+            fluid
+            roundedCircle
+            className="profile-image-edit"
+            id="recipe-profile-basic-info-image"
+          />
+          <FaCameraRetro className="fs-1 image-edit-cam text-black" />
+        </div>
+
+        <p>Insert image above</p>
+      </Row>
+    </div>
+  );
+}
--- src/Signup.tsx
+++ src/Signup.tsx
@@ -25,9 +25,44 @@ export default function Signup() {
       >
         Sign up
       </Link>
+    <div>
+      <h1>New here?</h1>
+      <div id="recipe-signup">
+        <h3>Sign up</h3>
+        <Form.Control id="recipe-name" placeholder="Name" className="mb-2" />
+        <Form.Control
+          id="recipe-username"
+          placeholder="Username"
+          className="mb-2"
+        />
+        <Form.Control
+          id="recipe-password"
+          placeholder="Password"
+          type="password"
+          className="mb-2"
+        />
+        <Form.Control
+          id="recipe-password-confirm"
+          placeholder="Confirm Password"
+          type="password"
+          className="mb-2"
+        />
+        <Form.Control
+          id="recipe-bio"
+          placeholder="About me"
+          className="mb-2"
+          as="textarea"
+        />
+        <Link
+          id="recipe-signin-btn"
+          to="/Profile"
+          className="btn btn-primary w-100 mb-2 bg-danger"
+        >
+          Sign up
+        </Link>
 
       <br />
-      <Link to="/Kambaz/Account/Signin" id="wd-signup-link">
+      <Link to="/Signin" id="recipe-signup-link">
         Sign in
       </Link>
     </div>
