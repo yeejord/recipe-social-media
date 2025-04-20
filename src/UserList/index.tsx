0ac40468c0cd5d2c36719ca0e6ca5392afaee42b
noah0young <drnoahyoung@gmail.com>
2025-04-20 19:49:31 -0400

    Hide admins from all users

--- src/UserList/index.tsx
+++ src/UserList/index.tsx
@@ -6,11 +6,13 @@ export default function UserList({ users }: { users: User[] }) {
   return (
     <div id="recipe-user-card-list">
       <Row>
-        {users.map((user) => (
-          <Col md={6} key={user._id}>
-            <UserCard user={user} key={user._id} />
-          </Col>
-        ))}
+        {users
+          .filter((u) => u.role !== "Admin")
+          .map((user) => (
+            <Col md={6} key={user._id}>
+              <UserCard user={user} key={user._id} />
+            </Col>
+          ))}
       </Row>
     </div>
   );
